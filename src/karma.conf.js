2f5901109cbe657773350f05c91e817584de8075
Pete <plamoni@katies-mbp.lan>
2020-11-27 23:25:38 -0800

    Fixing tests!

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -10,14 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'karma-typescript'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      '*.spec.ts',
-      '*.test.ts',
-      '**/*.test.ts'
+      '**/*.test.ts',
     ],
 
 
@@ -29,13 +27,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        '**/*.ts': 'karma-typescript'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'karma-typescript'],
 
 
     // web server port
@@ -57,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeCustom'],
+    browsers: [],
 
     // Custom launchers
     customLaunchers: {
