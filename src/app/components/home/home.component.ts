92c7b33b746b9be9777052ccc70f0a647bf108f1
plamoni <plamoni@gmail.com>
2020-11-29 13:04:22 -0800

    Setting up theme.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,22 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 /**
- *
+ * Home page.
  */
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  /**
-   *
-   */
-  constructor() { }
-
-  /**
-   *
-   */
-  ngOnInit(): void {
-  }
+export class HomeComponent {
 }
